5e630a6e8f9fec36fce3f77015d58a5a7a2eb8fd
CesarSotnas <devcarlos.cesar@gmail.com>
2025-10-04 16:55:56 -0300

    :hammer: add migrations and docker-compose file

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,75 @@
 import express from 'express';
+import * as mysql from 'mysql2/promise';
+import bcrypt from 'bcrypt';
+
+function createConnection(){
+    return mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'tickets'
+    })
+}
 
 const app = express();
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.json({message: "Hello World!"})
 });
+
+app.post('/auth/login', (req, res) => {
+    const {email, password} = req.body;
+    console.log(email, password);
+    res.send();
+})
+
+app.post('/partners', async (req, res) => {
+    const {name, email, password, company_name} = req.body;
+    const connection = await createConnection()
+    const createdAt = new Date();
+
+    connection.execute('INSERT INTO partners (name, email, password, created_at)',
+         [name, email,, createdAt])
+
+    connection.execute('INSERT INTO partners (user_id, company_name, created_at)', )
+})
+
+app.post('/partners/events', (req, res) => {
+    const {name, description, date, location} = req.body;
+    res.send()
+})
+
+app.get('/partners/events', (req, res) => {
+    res.send();
+})
+
+app.get('/partners/events/:eventId', (req, res) => {
+    const {eventId} = req.params
+    console.log(eventId)
+    res.send();
+})
+
+app.post('/customers', (req, res) => {
+    const {name, email, password, address, phone} = req.body;
+    console.log(name, email, password, address, phone)
+    res.send()
+})
+
+app.post('/events', (req, res) => {
+    const {name, description, date, location} = req.body;
+})
+
+app.get('/events', (req, res) => {
+    res.send();
+})
+
+app.get('/events/:eventId', (req, res) => {
+    const {eventId} = req.params
+    console.log(eventId)
+    res.send();
+})
 
 app.listen(3000, () => {
     console.log('Running in http://localhost:3000')
